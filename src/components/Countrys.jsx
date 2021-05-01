088709e3a890ccd8b5f50e671be9852affbe20cb
NilsonKr <nuotrok444@gmail.com>
2021-05-01 06:07:19 -0500

    :lipstick: [ADD] Flags Emoji and border gradient

--- src/components/Countrys.jsx
+++ src/components/Countrys.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Context } from '@context/ContextApp';
+import { countryCode } from 'emoji-flags';
 
 import '@styles/components/Countrys.css';
 
@@ -10,16 +11,18 @@ const Countrys = () => {
 	return (
 		<section className='countrys'>
 			<div className='countrys__container'>
-				<div className='countrys__head'>
-					<h2>Country</h2>
-					<h2>Total Cases</h2>
-					<h2>Total Deaths</h2>
-					<h2>Total Recovered</h2>
+				<div className='countrys__head--wrapper'>
+					<div className='countrys__head'>
+						<h2>Country</h2>
+						<h2>Total Cases</h2>
+						<h2>Total Deaths</h2>
+						<h2>Total Recovered</h2>
+					</div>
 				</div>
 				<div className='countrys__body'>
 					{data.Countries.map(countrie => (
 						<div className='country__info' key={countrie.ID}>
-							<h3>{countrie.CountryCode}</h3>
+							<h3>{countryCode(countrie.CountryCode).emoji}</h3>
 							<h3>{countrie.Country}</h3>
 							<h3>{countrie.TotalConfirmed.toLocaleString('de-DE')}</h3>
 							<h3>{countrie.TotalDeaths.toLocaleString('de-DE')}</h3>
